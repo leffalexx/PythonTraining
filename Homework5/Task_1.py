10822e1e9de4a499251515a30e8df0bcebbbade8
aleksei_namestnikov <leffalexx@gmail.com>
2022-09-22 09:46:11 +0300

    correction

--- Homework5/Task_1.py
+++ Homework5/Task_1.py
@@ -3,7 +3,6 @@
 string_1 = 'Мы неабв очень любим Питон иабв Джавабв'
 
 string_2 = 'абв'
-index = 0
 my_list = string_1.split(' ')
 res = [item for item in my_list if string_2 not in item]
 print((" ".join(res)))
