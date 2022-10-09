4faa2a03e07e275bb4558229bbd98d0078b7b2b8
Aleksei <leffalexx@gmail.com>
2022-10-09 20:21:00 +0300

    2nd done

---
+++ Homework6/Task_2.py
@@ -0,0 +1,22 @@
+# # 1. Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+# from random import randint
+
+# my_list = []
+# for i in range(10):
+#     my_list.append(randint(-100, 100))
+# print(my_list)
+
+# def oddsum(given_list):
+#     sum = 0
+#     for i in range(1, len(given_list), 2):
+#         sum += given_list[i]
+#     return sum
+# print(oddsum(my_list))
+
+from random import randint
+
+
+myList = [randint(0,11) for i in range (1,11)]
+print(myList)
+print(sum([myList[i] for i in range(1,len(myList),2)]))
